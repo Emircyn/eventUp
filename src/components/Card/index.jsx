7c8a589f847fb04c148c2628f55ad33dfac3c99b
Emircan Erdemci <Emircan.Erdemci@hotmail.com>
2022-12-24 00:21:43 +0300

    margin fix

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ import Button from '../Button';
 
 const Card = () => {
   return (
-    <div className="mb-10 flex flex-col items-center justify-around gap-9 rounded-lg  bg-cardBG bg-card-pattern bg-center py-14 px-6 lg:flex-row lg:px-6 lg:py-12">
+    <div className="mb-32 flex flex-col items-center justify-around gap-9 rounded-lg  bg-cardBG bg-card-pattern bg-center py-14 px-6 lg:flex-row lg:px-6 lg:py-12">
       <div className="flex max-w-lg flex-col gap-4">
         <h3 className="text-h3-custom !text-white">Ready to setup your next conference?</h3>
         <p className="text-neutral-50">
